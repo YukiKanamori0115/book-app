d9ca082cb8bb64e3b3b7975b7e9fad2f28eef8c7
namas-te-3 <dummy@example.com>
2026-05-28 15:40:24 +0900

    Breezeでログイン画面を作成

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,27 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-use App\Http\Controllers\BookController;
-
 // 「/books」にアクセスしたらBookControllerのindexという関数を動かす
 Route::get('/books', [BookController::class, 'index'])
-->middleware(['auth'])
-->name('books.index');
+    ->middleware(['auth'])
+    ->name('books.index');
 
+// ログイン画面
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
