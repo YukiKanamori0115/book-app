da5de18c890472c8729d21f97b75a5f5d67646ee
namas-te-3 <dummy@example.com>
2026-05-29 11:04:05 +0900

    migrationファイルとseederファイルの追加・修正

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,12 +10,22 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        // テストユーザー（社員）を登録
+        // テストユーザー（一般社員）を登録
         DB::table('users')->insert([
             'employee_id' => '0001',
             'name' => '山田太郎',
             'password' => Hash::make('password123'), // ログイン用パスワード
-            'role_id' => 1, // 1: 管理者
+            'role_id' => 2, // 2: 一般社員
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        // テストユーザー（経理部）を登録
+        DB::table('users')->insert([
+            'employee_id' => '0002',
+            'name' => '田中花子',
+            'password' => Hash::make('password123'), // ログイン用パスワード
+            'role_id' => 1, // 1: 経理部
             'created_at' => now(),
             'updated_at' => now(),
         ]);
