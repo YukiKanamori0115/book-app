8d32730ddabc4dfb2c51d5df5cfee3ca8c731a3b
shime <ryuushime@gmail.com>
2026-06-02 13:13:09 +0900

    syuusei

--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -22,7 +22,7 @@ class BookSeeder extends Seeder
 
         // 2種類目の本を登録
         Book::create([
-            'isbn13' => '9784274226298',
+            'isbn13' => '9780000000000',
             'title' => '達人プログラマー',
             'author' => '大阪花子',
         ]);
